2fd02838d6fc5398ac0682ecaacef6471b22e124
Amanda Lee Anderson <amandalee.anderson@gmail.com>
2016-05-20 15:52:44 -0500

    updates to some more stuff

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,19 @@
 class MessagesController < ApplicationController
 	def index
+		@messages = Message.all
 	end
 
 	def new 
+		@message = Message.new
+	end
+
+	def create
+		Message.create(message_params)
+		redirect_to message_path
+	end
+
+	private
+	def message_params
+		params.require(:message).permit(:title, :description)
 	end
 end
